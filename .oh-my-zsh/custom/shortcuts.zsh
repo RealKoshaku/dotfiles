b1f36b8ae37d8e0cd882fe275a6b96c5713a2941
Adahy Plas <plasadahy0@gmail.com>
2026-08-17 20:43:48 +0200

    feat: initial ZSH & Oh My Zsh config

---
+++ .oh-my-zsh/custom/shortcuts.zsh
@@ -0,0 +1,10 @@
+export THEMAC="koshaku@100.94.31.7"
+
+eval "$(zoxide init zsh)"
+
+alias ls="eza --icons"
+alias ll="eza -l --icons"
+alias la="eza -la --icons"
+alias connect_themacintosh="ssh koshaku@100.94.31.7"
+alias python=python3
+alias cat="bat"
